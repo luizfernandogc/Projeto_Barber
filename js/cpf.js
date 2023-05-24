48eff1717734f82d63564643cf2af474c5a07ed7
manodavi <Davitpontas@gmail.com>
2023-05-24 00:34:05 -0300

    'v1'

---
+++ js/cpf.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  $('#consultar').click(function () {
+    const cpf = $('#cpf').val();
+    if (cpf === '') {
+      alert('Por favor, digite um CPF.');
+      return;
+    }
+
+    // Realiza a requisição AJAX
+    $.ajax({
+      url: '../core/deletar.php',
+      method: 'POST',
+      data: { cpf: cpf },
+      success: function (response) {
+        $('#resultado').html(response);
+      },
+      error: function (xhr, status, error) {
+        console.error(error);
+        alert('CPF NÃO ENCONTRADO');
+      }
+    });
+  });
+
+  $('#deletar').click(function () {
+    const cpf = $('#cpf').val();
+    if (cpf === '') {
+      alert('Por favor, digite um CPF.');
+      return;
+    }
+
+    // Realiza a requisição AJAX para deletar o CPF
+    $.ajax({
+      url: '../core/deletar.php',
+      method: 'POST',
+      data: { cpf: cpf },
+      success: function (response) {
+        $('#resultado').html(response);
+      },
+      error: function (xhr, status, error) {
+        console.error(error);
+        alert('Erro ao deletar CPF.');
+      }
+    });
+  });
+});
